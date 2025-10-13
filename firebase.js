ed3c4ac958e15869cd3ef7a50daeeb00f88d039b
Sulaymaan Abubakr <abubakrysulaimaan24434@gmail.com>
2025-10-13 10:42:50 +0100

    updated firebase js

--- firebase.js
+++ firebase.js
@@ -12,7 +12,7 @@ import { getAuth, GoogleAuthProvider, signInWithPopup }
 // -------------------------------
 const firebaseConfig = {
   apiKey: "AIzaSyBQ74sMmOiYEvkxa26Movh0DAnmc0Jz60g",
-  authDomain: "yustam-50819.firebaseapp.com",
+  authDomain: "yustam.com.ng",
   projectId: "yustam-50819",
   storageBucket: "yustam-50819.appspot.com",  // ✅ fixed ".app" to ".appspot.com"
   messagingSenderId: "472601563195",
